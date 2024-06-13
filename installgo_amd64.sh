6dfbe3af065157c2917d803554162baffa0063f3
Andreas Vaaten <19690912+anvaa@users.noreply.github.com>
2024-06-13 09:11:59 +0200

    Update installgo_amd64.sh

--- installgo_amd64.sh
+++ installgo_amd64.sh
@@ -2,16 +2,24 @@
 
 # Update Go version to install
 # Find latest Go version https://go.dev/dl/
-VERSION=1.22.0
+VERSION=1.22.4
+
+# Select rpi version cpu
+CPU=arm64 #raspbian(rpi) 4/5, debian arm64
+#CPU=armv6l #rpi zw
+#CPU=amd64
+#CPU=386
+
+echo "Installing Go$VERSION for $CPU"
 
 ## Download the latest version of Golang
 echo "Downloading Go $VERSION"
-wget https://dl.google.com/go/go$VERSION.linux-amd64.tar.gz
+wget https://dl.google.com/go/go$VERSION.linux-$CPU.tar.gz
 echo "Downloading Go $VERSION completed"
 
 ## Extract the archive
 echo "Extracting..."
-tar -C ~/.local/share -xzf go$VERSION.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go$VERSION.linux-$CPU.tar.gz
 echo "Extraction complete"
 
 ## Detect the user's shell and add the appropriate path variables
@@ -19,21 +27,19 @@ SHELL_TYPE=$(basename "$SHELL")
 
 if [ "$SHELL_TYPE" = "zsh" ]; then
     echo "Found ZSH shell"
-    SHELL_RC="$HOME/.zshrc"
+    SHELL_RC=~/.zshrc
 elif [ "$SHELL_TYPE" = "bash" ]; then
     echo "Found Bash shell"
-    SHELL_RC="$HOME/.bashrc"
-elif [ "$SHELL_TYPE" = "fish" ]; then
-    echo "Found Fish shell"
-    SHELL_RC="$HOME/.config/fish/config.fish"
+    SHELL_RC=~/.bashrc
 else
     echo "Unsupported shell: $SHELL_TYPE"
     exit 1
 fi
 
-echo 'export GOPATH=$HOME/.go' >> "$SHELL_RC"
-echo 'export PATH=$HOME/.local/share/go/bin:$PATH' >> "$SHELL_RC"
-source .$SHELL_TYPE
+echo 'export GOPATH=$HOME/go' >> "$SHELL_RC"
+echo 'export PATH=/usr/local/go/bin:$PATH' >> "$SHELL_RC"
+echo 'export PATH=$HOME/go/bin:$PATH' >> "$SHELL_RC"
+source $SHELL_RC
 
 ## Verify the installation
 if [ -x "$(command -v go)" ]; then
@@ -48,4 +54,4 @@ else
 fi
 
 ## Clean up
-rm go$VERSION.linux-amd64.tar.gz
+rm go$VERSION.linux-$CPU.tar.gz
